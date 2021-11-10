c816b14842c6e03b9e954d5f776b6bf49e930ae0
Sparsh Kumar <sparshkumar14998@gmail.com>
2021-11-10 23:36:48 +0530

    added precondition and postcondition test cases for create todo api endpoint

--- test/spec/todo/todo.spec.ts
+++ test/spec/todo/todo.spec.ts
@@ -29,22 +29,68 @@ before(async () => {
 
 
 describe ('POST /todo',() => {
+  
+  let title = 'test title';
+  let todo;
 
   it ('should create a new todo with the title provided in the request body', async () => {
-    const res = await chai.request (expressApp).post ('/todo').send ({ title: 'test title' });
+
+    // Precondition
+    // No task should exist having title value 'test title'
+    todo = await testAppContext.todoRepository.findOne ({ title });
+    expect (JSON.stringify (todo)).to.equal ('{}');
+
+    // Perform Testing
+    // Trying to create task with title value 'test title'
+    const res = await chai.request (expressApp).post ('/todo').send ({ title });
     expect (res).to.have.status (201);
     expect (res.body).to.have.property ('id');
     expect (res.body).to.have.property ('title');
+
+    // Postcondition
+    // There should be a task created in DB with the title 'test title'
+    todo = await testAppContext.todoRepository.findOne ({ title });
+    expect (todo).to.have.property ('_id');
+    expect (todo).to.have.property ('title');
+
   })
 
   it ('should return validation error if no title is provided in the request body', async () => {
+
+    // Precondition
+    // Tasks with empty title should not exist
+    todo = await testAppContext.todoRepository.findOne ({ title: '' });
+    expect (JSON.stringify (todo)).to.equal ('{}');
+
+    // Perform Testing
+    // Trying to create a task having empty title
     const res = await chai.request (expressApp).post ('/todo').send ({ title: '' });
     expect (res).to.have.status (400);
+
+    // Postcondition
+    // There should not be any tasks with empty title
+    todo = await testAppContext.todoRepository.findOne ({ title: '' });
+    expect (JSON.stringify (todo)).to.equal ('{}');
   })
 
   it ('should return validation error if task with same title already exists', async () => {
-    const res = await chai.request (expressApp).post ('/todo').send ({ title: 'test title' });
+
+    // Precondition
+    // There should be a task present in DB having title 'test title'
+    todo = await testAppContext.todoRepository.findOne ({ title })
+    expect (todo).to.have.property ('_id');
+    expect (todo).to.have.property ('title');
+
+    // Perform Testing
+    // Trying to create another task having title 'test title'
+    const res = await chai.request (expressApp).post ('/todo').send ({ title });
     expect (res).to.have.status (400);
+
+    // Postcondition
+    // There should be only 1 task in Database having title 'test title'
+    todo = await testAppContext.todoRepository.getAll ({ title });
+    expect (todo).to.have.lengthOf (1);
+
   })
 
 })
